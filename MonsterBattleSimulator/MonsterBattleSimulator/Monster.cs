c3cab271916b1f9575cf6422044e4b7e1cc08199
K4K4tz3 <88493804+K4K4tz3@users.noreply.github.com>
2022-05-19 15:46:42 +0200

    Idk lol

---
+++ MonsterBattleSimulator/MonsterBattleSimulator/Monster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterBattleSimulator
+{
+    internal class Monster
+    {
+        internal float hp, ap, dp, s;
+        internal MonsterType type;
+        internal void Attack(Monster defender)
+        {
+            if (this.ap < defender.dp) return;
+            else defender.hp = defender.hp - (this.ap - defender.dp);
+            if (defender.hp < .0f) defender.hp = .0f;
+        }
+    }
+    enum MonsterType
+    {
+        Ork = 1,
+        Troll = 2,
+        Goblin = 3,
+    }
+}
+
---
+++ MonsterBattleSimulator/MonsterBattleSimulator/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MonsterBattleSimulator
+{
+    class Program
+    {
+        private static List<MonsterType> monsterTypes = new List<MonsterType>() { MonsterType.Ork, MonsterType.Troll, MonsterType.Goblin };
+        private static bool fight = true;
+        private static int rounds = 0;
+        private static int turn; // 0 -> Player/Monster1, 1 -> Enemy/Monster2
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Dear Challenger");
+            Thread.Sleep(500);
+            Console.WriteLine("I welcome you to the glorious Monster Battle Arenaaa");
+            Thread.Sleep(500);
+            Console.WriteLine("Please customize your Monster!");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Monster player = new Monster();
+            create_Character(player, "players");
+
+            Console.WriteLine("Please customize your opponents Monster!");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Monster enemy = new Monster();
+            create_Character(enemy, "opponents");
+
+            if (player.s > enemy.s) turn = 0; //the player starts the fight
+            else if (player.s < enemy.s) turn = 1; //the enemy starts the fight
+
+            Fight(player, enemy);
+        }
+        internal static void create_Character(Monster currentMonster, string character)
+        {
+            Console.WriteLine($"Choose your {character} monster:");
+            for (int i = 0; i < monsterTypes.Count; i++) Console.WriteLine($"{i + 1}. {monsterTypes[i]}");
+            int type = Convert.ToInt16(Console.ReadLine());
+            currentMonster.type = (MonsterType)type;
+            monsterTypes.RemoveAt(type - 1);
+            Console.WriteLine("How high are his attributes?");
+            Console.Write("Health: ");
+            currentMonster.hp = checkInput(Console.ReadLine(), "Health");
+            Console.Write("Attack Power: ");
+            currentMonster.ap = checkInput(Console.ReadLine(), "Attack Power");
+            Console.Write("Defence Power: ");
+            currentMonster.dp = checkInput(Console.ReadLine(), "Defence Power");
+            Console.Write("Speed: ");
+            currentMonster.s = checkInput(Console.ReadLine(), "Speed");
+            Thread.Sleep(500);
+            Console.Clear();
+        }
+        internal static float checkInput(string Input, string attribute)
+        {
+            float output;
+
+            try
+            {
+                if (float.Parse(Input) <= .0f) throw new ArgumentOutOfRangeException();
+                return output = float.Parse(Input);
+            }
+            catch
+            {
+                Console.WriteLine("That wasn't a valid input.");
+                Thread.Sleep(500);
+                Console.WriteLine("Please enter a positive number which is bigger than 0.");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.Write($"{attribute}: ");
+                checkInput(Console.ReadLine(), attribute);
+            }
+            return .0f;
+        }
+        internal static void Fight(Monster player, Monster enemy)
+        {
+            while (fight)
+            {
+                if (player.hp == .0f | enemy.hp == .0f)
+                {
+                    if (player.hp == .0f) Winner(enemy.type, rounds, enemy.hp);
+                    if (enemy.hp == .0f) Winner(player.type, rounds, player.hp);
+                    fight = false;
+                }
+                if (rounds == 100) aDraw();
+                else
+                {
+                    switch (turn)
+                    {
+                        case 0:
+                            player.Attack(enemy);
+                            turn = 1;
+                            break;
+                        case 1:
+                            enemy.Attack(player);
+                            turn = 0;
+                            break;
+                    }
+                    rounds++;
+                }
+            }
+        }
+        internal static void Winner(MonsterType winner, int roundsNeeded, float remainingHP)
+        {
+            Console.WriteLine($"The fight ended after {roundsNeeded} rounds.");
+            Thread.Sleep(500);
+            Console.WriteLine($"Our winner survived with {remainingHP} HP left.");
+            Thread.Sleep(500);
+            Console.WriteLine("But i know, you all are curious who the winner is.");
+            Thread.Sleep(500);
+            Console.Write($"Anndddd our winner is......");
+            Thread.Sleep(500);
+            Console.WriteLine($" the glorious {winner}!");
+            Console.ReadLine();
+        }
+        internal static void aDraw()
+        {
+            Console.WriteLine("After countless rounds...");
+            Thread.Sleep(500);
+            Console.WriteLine("Of brutal fights....");
+            Thread.Sleep(500);
+            Console.WriteLine("We sadly announce that we dont have a winner....");
+            Thread.Sleep(500);
+            Console.WriteLine("Both monsters presented huge power.");
+            Thread.Sleep(500);
+            Console.WriteLine("But they are on equal powerlevels.");
+            Thread.Sleep(500);
+        }
+    }
+}```
+
+---
+Tags:
+#Code
